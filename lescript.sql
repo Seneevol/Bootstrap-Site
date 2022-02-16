05585bba2661ccaa2d9c20e0efaa22c4db23610b
Seneevol <icire@hotmail.fr>
2022-02-16 15:07:59 +0100

    fix:Script SQL

--- lescript.sql
+++ lescript.sql
@@ -24,12 +24,13 @@ DROP TABLE IF EXISTS `articles`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `articles` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `articlename` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL,
+  `articlename` varchar(128) NOT NULL,
+  `image` varchar(510) NOT NULL,
+  `content` varchar(1024) NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `author_id` int DEFAULT NULL,
   `link` varchar(60) DEFAULT NULL,
+  `isVerify` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `author_id_idx` (`author_id`),
@@ -37,16 +38,6 @@ CREATE TABLE `articles` (
   CONSTRAINT `user_id` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `articles`
---
-
-LOCK TABLES `articles` WRITE;
-/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
-INSERT INTO `articles` VALUES (27,'Ouais','1644398742132_20220124_172425.jpg','nakad le teste','2022-02-07 11:02:25',24,'https://etoile-services.fr/'),(31,'Test','1644414256242_banner.jpg','C\'est moche','2022-02-09 14:44:16',24,'Le lien spotlify');
-/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `comments`
@@ -74,16 +65,6 @@ CREATE TABLE `comments` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `comments`
---
-
-LOCK TABLES `comments` WRITE;
-/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (1,'guhiodfig',24,27,'2022-02-08 16:04:48',NULL),(4,'ALL HAIL THE HERRSCHER OF SENTIENCE',29,27,'2022-02-09 11:07:42',NULL),(5,'Non.',24,27,'2022-02-09 11:07:58',NULL),(9,'Oui',24,27,'2022-02-09 14:42:54',5),(10,'J\'suis schyzo wola',29,27,'2022-02-09 14:43:08',5),(11,'No',24,27,'2022-02-09 14:43:17',5),(12,'Ca marche là aussi ou pas ?',24,27,'2022-02-09 14:48:26',4),(14,'Ich bin da',24,31,'2022-02-10 09:19:28',NULL),(15,'Guten morgen',24,31,'2022-02-10 09:19:37',14),(16,'Yo',24,27,'2022-02-16 11:53:45',NULL);
-/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `messages`
 --
 
@@ -101,15 +82,6 @@ CREATE TABLE `messages` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `messages`
---
-
-LOCK TABLES `messages` WRITE;
-/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `sessions`
 --
 
@@ -123,16 +95,6 @@ CREATE TABLE `sessions` (
   PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sessions`
---
-
-LOCK TABLES `sessions` WRITE;
-/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('ldK81uJVRdPb7rp_IbfM1tkw2uf7343V',1645095426,'{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"isAdmin\":true,\"user\":{\"id\":24,\"username\":\"Admin\",\"email\":\"yes@yes.yes\",\"avatar\":\"1644395765422_20220124_172425.jpg\",\"password\":\"$2b$10$L8k/8sjNYLC3dvf1dIfVK.bl31MEFO7Mc.dwflhnARpp6tRb7dEpO\",\"isAdmin\":1,\"isBan\":0}}'),('zG8q1tpok6-F5_IzPJWXMcBZCWJ_mGFv',1644930996,'{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"isAdmin\":true,\"user\":{\"id\":24,\"name\":\"Admin\",\"email\":\"yes@yes.yes\",\"avatar\":\"1644395765422_20220124_172425.jpg\",\"password\":\"$2b$10$L8k/8sjNYLC3dvf1dIfVK.bl31MEFO7Mc.dwflhnARpp6tRb7dEpO\",\"isAdmin\":1,\"isBan\":0}}');
-/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -155,16 +117,6 @@ CREATE TABLE `users` (
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (12,'< dev />','musk@tesla.drug','1643725282904_pnl.png','$2b$10$0T6IoOeSb7h.6zzr8DHSEub1HABG6fcQG8M1WQThC4bRAJQSTI7c2',0,0),(24,'Admin','yes@yes.yes','1644395765422_20220124_172425.jpg','$2b$10$L8k/8sjNYLC3dvf1dIfVK.bl31MEFO7Mc.dwflhnARpp6tRb7dEpO',1,0),(29,'Test','test@test.com','1644400567234_Capture d’écran du 2022-01-25 15-36-50.png','$2b$10$fXJ13jV.XEjzVNiQY1uf5eJZUMZQGLjSWpJlpFkUdR40gLSTzTEUq',0,0),(30,'Nakad','nakadcontact@gmail.com','default.png','$2b$10$UZBl12DifOWbVv1OHDnmReCg5Y1lXchZTYV/mXp.S8q/0YNPwYxyi',0,0),(31,'Yu','nakadmusic@gmail.com','default.png','$2b$10$rZhT1ALbdc1xq4TKwBEkEujWmVMRSYMRKeStKnEcTZO/6sYW6KfU6',0,0);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -175,4 +127,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-16 11:58:51
+-- Dump completed on 2022-02-16 15:04:10
